8df06dc36e2bb872633e93567724d6bcf10755bc
ArthasZhang007 <a1103753961@gmail.com>
2021-09-19 18:56:03 -0400

    update

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -13,14 +13,14 @@
 #include <random>
 
 GLuint hexapod_meshes_for_lit_color_texture_program = 0;
-Load< MeshBuffer > hexapod_meshes(LoadTagDefault, []() -> MeshBuffer const * {
+Load<MeshBuffer> hexapod_meshes(LoadTagDefault, []() -> MeshBuffer const * {
 	MeshBuffer const *ret = new MeshBuffer(data_path("playground.pnct"));
 	hexapod_meshes_for_lit_color_texture_program = ret->make_vao_for_program(lit_color_texture_program->program);
 	return ret;
 });
 
-Load< Scene > hexapod_scene(LoadTagDefault, []() -> Scene const * {
-	return new Scene(data_path("playground.scene"), [&](Scene &scene, Scene::Transform *transform, std::string const &mesh_name){
+Load<Scene> hexapod_scene(LoadTagDefault, []() -> Scene const * {
+	return new Scene(data_path("playground.scene"), [&](Scene &scene, Scene::Transform *transform, std::string const &mesh_name) {
 		Mesh const &mesh = hexapod_meshes->lookup(mesh_name);
 
 		scene.drawables.emplace_back(transform);
@@ -32,20 +32,22 @@ Load< Scene > hexapod_scene(LoadTagDefault, []() -> Scene const * {
 		drawable.pipeline.type = mesh.type;
 		drawable.pipeline.start = mesh.start;
 		drawable.pipeline.count = mesh.count;
-
 	});
 });
 
-PlayMode::PlayMode() : scene(*hexapod_scene) {
+PlayMode::PlayMode() : scene(*hexapod_scene)
+{
 	//get pointers to leg for convenience:
-	for (auto &transform : scene.transforms) {
-		std::cout<<transform.name<<std::endl;
+	for (auto &transform : scene.transforms)
+	{
+		std::cout << transform.name << std::endl;
 		if (transform.name == "player1")
 		{
-			player1_t = &transform;
+			player1_t.cur = &transform;
 		}
-			
-		if (transform.name == "arrow"){
+
+		if (transform.name == "arrow")
+		{
 			arrow1_t = &transform;
 			arrow_base_rotation = arrow1_t->rotation;
 		}
@@ -62,66 +64,100 @@ PlayMode::PlayMode() : scene(*hexapod_scene) {
 	lower_leg_base_rotation = lower_leg->rotation;*/
 
 	//get pointer to camera for convenience:
-	if (scene.cameras.size() != 1) throw std::runtime_error("Expecting scene to have exactly one camera, but it has " + std::to_string(scene.cameras.size()));
+	if (scene.cameras.size() != 1)
+		throw std::runtime_error("Expecting scene to have exactly one camera, but it has " + std::to_string(scene.cameras.size()));
 	camera = &scene.cameras.front();
 }
 
-PlayMode::~PlayMode() {
+PlayMode::~PlayMode()
+{
 }
 
-bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size) {
+bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
+{
 
-	if (evt.type == SDL_KEYDOWN) {
-		if (evt.key.keysym.sym == SDLK_ESCAPE) {
+	if (evt.type == SDL_KEYDOWN)
+	{
+		if (evt.key.keysym.sym == SDLK_ESCAPE)
+		{
 			SDL_SetRelativeMouseMode(SDL_FALSE);
 			return true;
-		} else if (evt.key.keysym.sym == SDLK_a) {
+		}
+		else if (evt.key.keysym.sym == SDLK_a)
+		{
 			left.downs += 1;
 			left.pressed = true;
 			return true;
-		} else if (evt.key.keysym.sym == SDLK_d) {
+		}
+		else if (evt.key.keysym.sym == SDLK_d)
+		{
 			right.downs += 1;
 			right.pressed = true;
 			return true;
-		} else if (evt.key.keysym.sym == SDLK_w) {
+		}
+		else if (evt.key.keysym.sym == SDLK_w)
+		{
 			up.downs += 1;
 			up.pressed = true;
 			return true;
-		} else if (evt.key.keysym.sym == SDLK_s) {
+		}
+		else if (evt.key.keysym.sym == SDLK_s)
+		{
 			down.downs += 1;
 			down.pressed = true;
 			return true;
 		}
-	} else if (evt.type == SDL_KEYUP) {
-		if (evt.key.keysym.sym == SDLK_a) {
+		else if (evt.key.keysym.sym == SDLK_SPACE)
+		{
+			if (player1_t.pushable == true)
+			{
+				player1_t.pushable = false;
+				float angle = 2.0f * float(M_PI) * wobble;
+				glm::vec3 dir(cos(angle), sin(angle), 0.0f);
+				player1_t.push(extend(dir, -2.0f));
+			}
+		}
+	}
+	else if (evt.type == SDL_KEYUP)
+	{
+		if (evt.key.keysym.sym == SDLK_a)
+		{
 			left.pressed = false;
 			return true;
-		} else if (evt.key.keysym.sym == SDLK_d) {
+		}
+		else if (evt.key.keysym.sym == SDLK_d)
+		{
 			right.pressed = false;
 			return true;
-		} else if (evt.key.keysym.sym == SDLK_w) {
+		}
+		else if (evt.key.keysym.sym == SDLK_w)
+		{
 			up.pressed = false;
 			return true;
-		} else if (evt.key.keysym.sym == SDLK_s) {
+		}
+		else if (evt.key.keysym.sym == SDLK_s)
+		{
 			down.pressed = false;
 			return true;
 		}
-	} else if (evt.type == SDL_MOUSEBUTTONDOWN) {
-		if (SDL_GetRelativeMouseMode() == SDL_FALSE) {
+	}
+	else if (evt.type == SDL_MOUSEBUTTONDOWN)
+	{
+		if (SDL_GetRelativeMouseMode() == SDL_FALSE)
+		{
 			SDL_SetRelativeMouseMode(SDL_TRUE);
 			return true;
 		}
-	} else if (evt.type == SDL_MOUSEMOTION) {
-		if (SDL_GetRelativeMouseMode() == SDL_TRUE) {
+	}
+	else if (evt.type == SDL_MOUSEMOTION)
+	{
+		if (SDL_GetRelativeMouseMode() == SDL_TRUE)
+		{
 			glm::vec2 motion = glm::vec2(
 				evt.motion.xrel / float(window_size.y),
-				-evt.motion.yrel / float(window_size.y)
-			);
+				-evt.motion.yrel / float(window_size.y));
 			camera->transform->rotation = glm::normalize(
-				camera->transform->rotation
-				* glm::angleAxis(-motion.x * camera->fovy, glm::vec3(0.0f, 1.0f, 0.0f))
-				* glm::angleAxis(motion.y * camera->fovy, glm::vec3(1.0f, 0.0f, 0.0f))
-			);
+				camera->transform->rotation * glm::angleAxis(-motion.x * camera->fovy, glm::vec3(0.0f, 1.0f, 0.0f)) * glm::angleAxis(motion.y * camera->fovy, glm::vec3(1.0f, 0.0f, 0.0f)));
 			return true;
 		}
 	}
@@ -129,18 +165,22 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 	return false;
 }
 
-void PlayMode::update(float elapsed) {
+void PlayMode::update(float elapsed)
+{
 
 	//slowly rotates through [0,1):
 	wobble += elapsed / 10.0f;
 	wobble -= std::floor(wobble);
 
-	//player1_t->position.z += 0.02f;
-	arrow1_t->rotation = arrow_base_rotation * glm::angleAxis(
-		2.0f * float(M_PI) * wobble, 
-		//glm::radians(7.0f * std::sin(wobble * 2.0f * 2.0f * float(M_PI))),
-		glm::vec3(0.0f, 0.0f, 1.0f)
-	); 
+	float angle = 2.0f * float(M_PI) * wobble;
+
+	if (player1_t.pushable)
+	{
+		arrow1_t->rotation = arrow_base_rotation * glm::angleAxis(
+													   angle,
+													   glm::vec3(0.0f, 0.0f, 1.0f));
+	}
+	player1_t.update(elapsed);
 	/*hip->rotation = hip_base_rotation * glm::angleAxis(
 		glm::radians(5.0f * std::sin(wobble * 2.0f * float(M_PI))),
 		glm::vec3(0.0f, 1.0f, 0.0f)
@@ -160,13 +200,18 @@ void PlayMode::update(float elapsed) {
 		//combine inputs into a move:
 		constexpr float PlayerSpeed = 30.0f;
 		glm::vec2 move = glm::vec2(0.0f);
-		if (left.pressed && !right.pressed) move.x =-1.0f;
-		if (!left.pressed && right.pressed) move.x = 1.0f;
-		if (down.pressed && !up.pressed) move.y =-1.0f;
-		if (!down.pressed && up.pressed) move.y = 1.0f;
+		if (left.pressed && !right.pressed)
+			move.x = -1.0f;
+		if (!left.pressed && right.pressed)
+			move.x = 1.0f;
+		if (down.pressed && !up.pressed)
+			move.y = -1.0f;
+		if (!down.pressed && up.pressed)
+			move.y = 1.0f;
 
 		//make it so that moving diagonally doesn't go faster:
-		if (move != glm::vec2(0.0f)) move = glm::normalize(move) * PlayerSpeed * elapsed;
+		if (move != glm::vec2(0.0f))
+			move = glm::normalize(move) * PlayerSpeed * elapsed;
 
 		glm::mat4x3 frame = camera->transform->make_local_to_parent();
 		glm::vec3 right = frame[0];
@@ -183,7 +228,8 @@ void PlayMode::update(float elapsed) {
 	down.downs = 0;
 }
 
-void PlayMode::draw(glm::uvec2 const &drawable_size) {
+void PlayMode::draw(glm::uvec2 const &drawable_size)
+{
 	//update camera aspect ratio for drawable:
 	camera->aspect = float(drawable_size.x) / float(drawable_size.y);
 
@@ -191,7 +237,7 @@ void PlayMode::draw(glm::uvec2 const &drawable_size) {
 	// TODO: consider using the Light(s) in the scene to do this
 	glUseProgram(lit_color_texture_program->program);
 	glUniform1i(lit_color_texture_program->LIGHT_TYPE_int, 1);
-	glUniform3fv(lit_color_texture_program->LIGHT_DIRECTION_vec3, 1, glm::value_ptr(glm::vec3(0.0f, 0.0f,-1.0f)));
+	glUniform3fv(lit_color_texture_program->LIGHT_DIRECTION_vec3, 1, glm::value_ptr(glm::vec3(0.0f, 0.0f, -1.0f)));
 	glUniform3fv(lit_color_texture_program->LIGHT_ENERGY_vec3, 1, glm::value_ptr(glm::vec3(1.0f, 1.0f, 0.95f)));
 	glUseProgram(0);
 
@@ -213,18 +259,17 @@ void PlayMode::draw(glm::uvec2 const &drawable_size) {
 			1.0f / aspect, 0.0f, 0.0f, 0.0f,
 			0.0f, 1.0f, 0.0f, 0.0f,
 			0.0f, 0.0f, 1.0f, 0.0f,
-			0.0f, 0.0f, 0.0f, 1.0f
-		));
+			0.0f, 0.0f, 0.0f, 1.0f));
 
 		constexpr float H = 0.09f;
 		lines.draw_text("Mouse motion rotates camera; WASD moves; escape ungrabs mouse",
-			glm::vec3(-aspect + 0.1f * H, -1.0 + 0.1f * H, 0.0),
-			glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
-			glm::u8vec4(0x00, 0x00, 0x00, 0x00));
+						glm::vec3(-aspect + 0.1f * H, -1.0 + 0.1f * H, 0.0),
+						glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
+						glm::u8vec4(0x00, 0x00, 0x00, 0x00));
 		float ofs = 2.0f / drawable_size.y;
 		lines.draw_text("Mouse motion rotates camera; WASD moves; escape ungrabs mouse",
-			glm::vec3(-aspect + 0.1f * H + ofs, -1.0 + + 0.1f * H + ofs, 0.0),
-			glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
-			glm::u8vec4(0xff, 0xff, 0xff, 0x00));
+						glm::vec3(-aspect + 0.1f * H + ofs, -1.0 + +0.1f * H + ofs, 0.0),
+						glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
+						glm::u8vec4(0xff, 0xff, 0xff, 0x00));
 	}
 }
--- PlayMode.hpp
+++ PlayMode.hpp
@@ -1,11 +1,36 @@
 #include "Mode.hpp"
-
 #include "Scene.hpp"
 
 #include <glm/glm.hpp>
 
 #include <vector>
 #include <deque>
+namespace {
+const float deacc_const = 0.01f;
+const float ball_radius = 0.5f;
+struct ball{
+	glm::vec3 velocity;
+	Scene::Transform *cur;
+	bool pushable;
+	ball(){velocity = glm::vec3(); cur = nullptr; pushable = true;}
+	void update(float elapsed){
+		cur->position += velocity * elapsed;
+		velocity *= (1.0f - deacc_const * elapsed);
+	}
+	void push(glm::vec3 acc){
+		velocity += acc;
+	}
+};
+float len(glm::vec3 x){
+	return std::sqrt(x.x*x.x+x.y*x.y+x.z*x.z);
+}
+glm::vec3 extend(glm::vec3 x, float k){
+	return x / len(x) * k;
+}
+void collide(ball &ball_A, ball & ball_B){
+
+}
+}
 
 struct PlayMode : Mode {
 	PlayMode();
@@ -33,7 +58,7 @@ struct PlayMode : Mode {
 	Scene::Transform *lower_leg = nullptr;
 
 	//self-defined
-	Scene::Transform *player1_t = nullptr;
+	ball player1_t;
 	Scene::Transform *arrow1_t = nullptr;
 	glm::quat arrow_base_rotation;
 
